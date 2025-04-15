fa26ec50927b65d69db4259947065159c4f3ad75
Matteo Forlani <matteo@matthew5pl.net>
2025-04-15 22:48:43 +0200

    simplify bitwise operations in matrix scanning code

--- qmk/capa/keymaps/default/matrix.c
+++ qmk/capa/keymaps/default/matrix.c
@@ -10,17 +10,7 @@ bool matrix_scan_custom(matrix_row_t mat[]) {
     bool pressed_left = capacitive_pressed(PIN_LEFT);
     bool pressed_right = capacitive_pressed(PIN_RIGHT);
 
-    if(pressed_left) {
-        mat[0] |= 0b01;
-    } else {
-        mat[0] &= 0b10;
-    }
-    
-    if(pressed_right) {
-        mat[0] |= 0b10;
-    } else {
-        mat[0] &= 0b01;
-    }   
+    mat[0] = (pressed_right << 1) | pressed_left;   
     
     bool changed = mat[0] != last_row;
     last_row = mat[0];
